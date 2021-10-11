3d29d5663df9ff272b2c530ae4c76e8888f1d641
HorseScary <horsescary@gmail.com>
2021-10-11 15:10:25 -0400

    did some stuff idk

--- quartiletest.py
+++ quartiletest.py
@@ -1,6 +1,6 @@
 from sorting import sort, quartile
 
 
-sortdat = [0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 9, 9]
+sortdat = [0, 1, 1, 1, 1, 1, 2, 3, 4, 4, 4, 5, 6, 6, 8, 8, 8, 9, 9, 10]
 
 print(quartile(sortdat))
--- sorting.py
+++ sorting.py
@@ -5,16 +5,23 @@ do control c to get out!
 
 def sort(data):
     while True:
+
         for i in range(len(data)):
             hold = 0
             if i == len(data) -1:
                 break
+#            check = data
+            
             if data[i] > data[i+1]:
                 hold = data[i+1]
                 data[i+1] = data[i]
                 data[i] = hold
+                print(data)
 
-            print (data)
+#            print (f"data: {data}\ncheck:{check}")
+        
+#        if check == data:
+#            break
 
     return(data)
 
