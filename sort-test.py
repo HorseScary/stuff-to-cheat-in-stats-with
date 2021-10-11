cf197fc170d4685338a0cfa2a51f98f143240fdd
HorseScary <horsescary@gmail.com>
2021-10-11 14:42:00 -0400

    first commit

---
+++ sort-test.py
@@ -0,0 +1,5 @@
+from sorting import sort, quartile
+
+
+data = [1,5,3,7,8,8,6,5,0,8,2,4,9,6,8,1,0,1,8,9,1,7,2,4,6,0,4,6]
+print(sort(data))
---
+++ sorting.py
@@ -0,0 +1,34 @@
+"""
+i cant be asked to acutaly check if the thing is sorted so it just goes infinitly 
+do control c to get out!
+"""
+
+def sort(data):
+    while True:
+        for i in range(len(data)):
+            hold = 0
+            if i == len(data) -1:
+                break
+            if data[i] > data[i+1]:
+                hold = data[i+1]
+                data[i+1] = data[i]
+                data[i] = hold
+
+            print (data)
+
+    return(data)
+
+def quartile(data):
+
+    if len(data)%2 == 1:
+        q2 = data[int((len(data)/2)-.5)]
+
+    
+    else:
+        a = int(len(data)/2)
+        b = int(len(data)/4)
+        q1 = (data[b] + data[b-1])/2
+        q2 = (data[a] + data[a-1])/2
+        q3 = (data[a+b] + data[a+b-1])/2
+
+    return(f"First Quartile: {q1}\nSecond Quartile: {q2}\nThird Quartile: {q3}")
