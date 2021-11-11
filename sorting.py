1c143528bc47efd657a954ec5df2497c868a600e
HorseScary <horsescary@gmail.com>
2021-11-11 17:29:54 -0500

    sorting alg w recursion

--- sorting.py
+++ sorting.py
@@ -10,7 +10,6 @@ def sort(data):
             hold = 0
             if i == len(data) -1:
                 break
-#            check = data
             
             if data[i] > data[i+1]:
                 hold = data[i+1]
@@ -18,9 +17,27 @@ def sort(data):
                 data[i] = hold
                 print(data)
 
-#            print (f"data: {data}\ncheck:{check}")
-        
-#        if check == data:
-#            break
 
-    return(data)
+def sortRecursive(data):
+    check = data[:]
+    def sortReal(data, check, it):
+        it +=1
+        for i in range(len(data)):
+            hold = 0
+            if i == len(data) -1:
+                break
+            
+            if data[i] > data[i+1]:
+                hold = data[i+1]
+                data[i+1] = data[i]
+                data[i] = hold
+
+
+        if check == data:
+            return(f"[LIST SORTED]\n{data}\nIt took {it} tries to sort your list!")
+        
+        else:
+            check = data[:]
+            return(sortReal(data, check, it))
+    
+    return(sortReal(data, check, 0))
