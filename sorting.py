224156fff7067a2c1ab415d9019fb001ffe34d11
HorseScary <horsescary@gmail.com>
2021-11-11 16:41:20 -0500

    moved code for finding quartiles from sorting to misc

--- sorting.py
+++ sorting.py
@@ -24,21 +24,3 @@ def sort(data):
 #            break
 
     return(data)
-
-def quartile(data):
-
-    if len(data)%2 == 1:
-        a = int((len(data)/2)-.5)
-        b = int((len(data)/4)-.5)
-        q2 = data[a]
-        q1 = data[b]
-        q3 = data[a+b]
-    
-    else:
-        a = int(len(data)/2)
-        b = int(len(data)/4)
-        q1 = (data[b] + data[b-1])/2
-        q2 = (data[a] + data[a-1])/2
-        q3 = (data[a+b] + data[a+b-1])/2
-
-    return(f"First Quartile: {q1}\nSecond Quartile: {q2}\nThird Quartile: {q3}")
