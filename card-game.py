c6ee800f7aaf483a8b0f2cfecf00af436752fa19
HorseScary <horsescary@gmail.com>
2021-11-10 21:13:50 -0500

    made some changes to card game, updated git ignore

--- card-game.py
+++ card-game.py
@@ -1,28 +1,25 @@
 """
 WAYS TO WIN
-acending order
+ascending order
 3 of the same suite 
 all face cards
 """
-
 from deckOfCards import getCard
+def draw():
+    card1 = getCard()
+    card2 = getCard()
+    card3 = getCard()
 
-card1 = getCard()
-card2 = getCard()
-card3 = getCard()
+    while card1 == card2 or card1 == card3 or card2 == card3:
 
-while card1 == card2 or card1 == card3 or card2 == card3:
+        print ("This ran")
 
-    if card1 == card2 or card3:
-        card1 == getCard()
-    
-    elif card2 == card3 or card1:
-        card2 == getCard()
-    else:
-        print("Why are we here?")
-    print ("owo")
+        if card1 == card2 or card3:
+            card1 = getCard()
+        
+        elif card2 == card3 or card1:
+            card2 = getCard()
 
-if card1[1] == 'a' or 'j' or'k' or 'q':
-    print("Face card!")
-
-print ('a')
+def winning(card1, card2, card3):
+    if card1[1] == 'a' or 'j' or 'k' or 'q' and card2[1] == 'a' or 'j' or 'k' or 'q' and card3[1]=='a' or 'j' or 'k' or 'q' :
+        print("3 face cards!")
