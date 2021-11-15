a461cf37370694c4e0882529314dc9cafb3836bf
HorseScary <jakobaayers@gmial.com>
2021-11-15 10:38:32 -0500

    a

--- distrabution.py
+++ distrabution.py
@@ -1,11 +1,8 @@
 def freqDistrabution(data):
     total = 0
     distrabution = []
-
     for i in range(len(data)):
         total += data[i]
-    
     for i in range(len(data)):
         distrabution.append(data[i] / total)
-    
     return(distrabution)
